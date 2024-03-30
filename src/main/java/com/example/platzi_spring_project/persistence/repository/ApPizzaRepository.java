57f5d6f251538cf746bf9272ed0fec6fb4f6801e
adisonzenemij <adisonzenemij@gmail.com>
2024-03-30 16:29:36 -0500

    Feat: Modulos Ap Eliminados

--- src/main/java/com/example/platzi_spring_project/persistence/repository/ApPizzaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.platzi_spring_project.persistence.repository;
-
-import com.example.platzi_spring_project.persistence.entity.ApPizzaEntity;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface ApPizzaRepository extends ListCrudRepository<ApPizzaEntity, Integer> {
-}
