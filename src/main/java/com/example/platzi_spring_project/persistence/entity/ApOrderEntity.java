a3a9ff3caebe8dd4347b85e753c0eb47a1a76cac
adisonzenemij <adisonzenemij@gmail.com>
2024-03-30 15:20:29 -0500

    Feat: Modulo Ap Order

--- src/main/java/com/example/platzi_spring_project/persistence/entity/ApOrderEntity.java
+++ src/main/java/com/example/platzi_spring_project/persistence/entity/ApOrderEntity.java
@@ -2,12 +2,7 @@ package com.example.platzi_spring_project.persistence.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
