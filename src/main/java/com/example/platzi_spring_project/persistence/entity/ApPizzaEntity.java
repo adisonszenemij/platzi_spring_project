3cd067161061250ef929c3e8ea0469d6d47a7e43
adisonzenemij <adisonzenemij@gmail.com>
2024-03-30 15:12:08 -0500

    Feat: Modulo Ap Order

--- src/main/java/com/example/platzi_spring_project/persistence/entity/ApPizzaEntity.java
+++ src/main/java/com/example/platzi_spring_project/persistence/entity/ApPizzaEntity.java
@@ -1,2 +1,34 @@
-package com.example.platzi_spring_project.persistence.entity;public class ApPizzaEntity {
+package com.example.platzi_spring_project.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ap_pizza")
+@Getter
+@Setter
+public class ApPizzaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_register", nullable = false)
+    private Integer idRegister;
+
+    @Column(name = "cd_name", nullable = false, unique = false, length = 255)
+    private String cdName;
+
+    @Column(name = "cd_description", nullable = false, unique = false, length = 255)
+    private String cdDescription;
+
+    @Column(name = "cd_price", nullable = false, unique = false, columnDefinition = "Decimal(5,2)")
+    private Double cdPrice;
+
+    @Column(name = "cd_vegetarian", nullable = false, unique = false, columnDefinition = "TINYINT")
+    private Boolean cdVegetarian;
+
+    @Column(name = "cd_vegan", nullable = false, unique = false, columnDefinition = "TINYINT")
+    private Boolean cdVegan;
+
+    @Column(name = "cd_available", nullable = false, unique = false, columnDefinition = "TINYINT")
+    private Boolean cdAvailable;
 }
--- src/main/java/com/example/platzi_spring_project/persistence/entity/OrderEntity.java
+++ src/main/java/com/example/platzi_spring_project/persistence/entity/OrderEntity.java
@@ -1,2 +1,30 @@
-package com.example.platzi_spring_project.persistence.entity;public class OrderEntity {
+package com.example.platzi_spring_project.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ap_order")
+@Getter
+@Setter
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_register", nullable = false)
+    private Integer idRegister;
+
+    @Column(name = "cd_date", nullable = false, unique = false, columnDefinition = "DATETIME")
+    private LocalDateTime cdDate;
+
+    @Column(name = "cd_total", nullable = false, unique = false, columnDefinition = "Decimal(6,2)")
+    private Double cdTotal;
+
+    @Column(name = "cd_method", nullable = false, unique = false, columnDefinition = "CHAR(1)")
+    private String cdMethod;
+
+    @Column(name = "cd_additional", nullable = false, unique = false, length = 255)
+    private String cdAdditional;
 }
