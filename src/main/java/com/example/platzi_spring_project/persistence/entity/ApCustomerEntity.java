063edef5cef27e6dd815e562c7f49d90b4d28c67
adisonzenemij <adisonzenemij@gmail.com>
2024-03-30 15:21:11 -0500

    Feat: Modulo Ap Customer

--- src/main/java/com/example/platzi_spring_project/persistence/entity/ApCustomerEntity.java
+++ src/main/java/com/example/platzi_spring_project/persistence/entity/ApCustomerEntity.java
@@ -2,6 +2,7 @@ package com.example.platzi_spring_project.persistence.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Table(name = "ap_customer")
 @Getter
 @Setter
+@NoArgsConstructor
 public class ApCustomerEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
