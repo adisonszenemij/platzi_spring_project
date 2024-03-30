f70175c967c6a224a067a3c3f1c6f87e4bca88e1
adisonzenemij <adisonzenemij@gmail.com>
2024-03-30 14:16:56 -0500

    Feat: Proyecto Desarrollado

---
+++ src/main/java/com/example/platzi_spring_project/PlatziSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.platzi_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/platzi_spring_project/PlatziSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platzi_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
