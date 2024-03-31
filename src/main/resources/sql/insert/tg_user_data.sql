ab2411a6a6cc9b89555d3fe4b4e7cc4596e8a080
adisonzenemij <adisonzenemij@gmail.com>
2024-03-30 23:50:42 -0500

    Feat: Base de Datos Sentencias

--- src/main/resources/sql/insert/tg_user_data.sql
+++ src/main/resources/sql/insert/tg_user_data.sql
@@ -1,4 +1,4 @@
 -- Insertar registros en la tabla
 INSERT INTO `bd_platzi_spring_project`.`tg_user_data` (`at_created_date`, `cd_email`, `cd_login`, `cd_password`, `tg_role_data`) VALUES
-(NOW(), 'root@adisonjimenez.work', 'root', '', '1'),
-(NOW(), 'devp@adisonjimenez.work', 'devp', '', '2');
+(NOW(), 'root@adisonjimenez.work', 'root', '$2y$10$oLBBmj6mVyUUXZv2aPjdVOb9AQrO0SsdipIdP0fNgiZrlNWJ9qY86', '1'),
+(NOW(), 'devp@adisonjimenez.work', 'devp', '$2y$10$RL0bMtr8zhnRrErjTRW4S.wauWVg5qdAIGuztb8qmctxwSu/8fSf6', '2');
